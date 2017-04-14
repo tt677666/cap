671959ce6d69a4efcae7c9d2567f519fd784a386
tt677666 <tt677666@gmail.com>
2017-04-14 17:42:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from scapy.all import *
 from scapy.layers import http
 import struct
-next_ack = 0
+next_ack = -1
 tcp_sport = 0
 dataflow = []
 def check_ack(pkt,ensure_ack):
@@ -20,9 +20,9 @@ def http_header(packet):
 			print http_layer.fields['Host']+http_layer.fields['Path']
 			#print len(packet) + 1,type(len(packet))
 			#print packet[TCP].seq,type(packet[TCP].seq)
- 			next_ack = len(packet) + 1 - 54 + packet[TCP].seq
-			#print 'next_ack is',next_ack
-			tcp_sport = packet[TCP].sport
+ 			next_ack = len(packet) - 54 + packet[TCP].seq
+			print 'next_ack is',next_ack
+			#tcp_sport = packet[TCP].sport
 	#print 'seq is ',packet[TCP].seq
 	#if next_ack ==  packet[TCP].seq and tcp_sport == packet[TCP].sport:
 		#print 'flag is ',packet[TCP].flags
@@ -32,11 +32,15 @@ def http_header(packet):
 			#bytes=struct.pack('i',hexdump(packet))
 			file_object=open('dy.flv','a')
 			print 1111111111111111222222222222
-			file_object.write(str(packet))
+			print packet[TCP].payload,type(packet[TCP].payload)
+			file_object.write(packet[TCP].payload)
 			file_object.close()
-			dataflow.append(hexdump(packet))
+			#dataflow.append(hexdump(packet))
 		if packet.haslayer(http.HTTPResponse):
-			dataflow = []
+		    http_layer1 = packet.getlayer(http.HTTPResponse)
+		    if str(http_layer1).find('302'):
+				dataflow = []
+				print 'the list had been reset'
 			
 
-packet=sniff(filter='tcp port 80',prn=http_header,store=0) 
+packet=sniff(filter='tcp port 80',prn=http_header,store=0)
