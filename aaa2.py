e7a9b4e6550ebcc27625492bce9f2fbc9bd1c097
tt677666 <tt677666@gmail.com>
2017-06-15 17:33:16 +0800

    Update aaa2.py

--- aaa2.py
+++ aaa2.py
@@ -8,17 +8,23 @@ print sys.getdefaultencoding()
 from scapy.all import *
 from scapy.layers import http
 
+#'========================global================================='
 file_name='tmp'
 file_info={}
 file_info['video_tmp0'] = 0
 file_info['video_tmp1'] = 0
-
-os.remove('video_tmp0')
-os.remove('video_tmp1')
+pre_len = 0
+video_list = []
+#'========================global================================='
+time.sleep(1)
+if os.path.exists('video_tmp0'):
+	os.remove('video_tmp0')
+if os.path.exists('video_tmp1'):
+	os.remove('video_tmp1')
 
 subprocess.Popen('tcpdump.exe -C 10 -W 2 -n -nn -f -s 0 -w video_tmp  tcp port 80',shell=True)
 
-time.sleep(1)
+
 
 
 def check_file_change():
@@ -28,34 +34,48 @@ def check_file_change():
 			file0_size = os.path.getsize('video_tmp0')
 			if  file0_size - file_info['video_tmp0'] > 0 or file0_size - file_info['video_tmp0'] < 0:
 				file_info['video_tmp0'] = file0_size
-				print 'video_tmp0'
 				return 'video_tmp0'
 				break
 		if os.path.exists('video_tmp1'):
 			file1_size = os.path.getsize('video_tmp1')
 			if file1_size - file_info['video_tmp1'] > 0 or file1_size - file_info['video_tmp1'] < 0:
 				file_info['video_tmp1'] = file1_size
-				print 'video_tmp1'
 				return 'video_tmp1'
 				break
 		else:
 			continue
 
 def read_pcap(pcap_name):
+	global pre_len,file_name,video_list
+	
+	if file_name != pcap_name:
+		pre_len = 0
+		file_name = pcap_name
+		
 	pkt = rdpcap(pcap_name)
+	#video_list.append(len(video_list):)
+	for k in video_list[len(video_list):len(pkt)]:
+		video_list.append(k)
+	if len(pkt) > pre_len:
+		
+		a = 'read more part'
+		pre_len = len(pkt)
+	
 	print len(pkt),pcap_name
+	
+	
 
 	
 
 		
 
 def check_target_change(target_file):
-	global file_name
-	if file_name != target_file:
-		read_pcap(target_file)
-		file_name = target_file
-	else:
-		pass
+	#global file_name
+	#if file_name != target_file:
+	read_pcap(target_file)
+		#file_name = target_file
+	#else:
+		#pass
 
 while True:
 
